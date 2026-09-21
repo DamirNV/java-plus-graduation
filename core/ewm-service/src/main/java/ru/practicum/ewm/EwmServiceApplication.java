2ade260604859afa191358839727007c8539374e
Damir Vafin <damirvafinnailevich@gmail.com>
2026-09-21 15:10:15 +0300

    fix: корректно подключить клиент статистики

--- core/ewm-service/src/main/java/ru/practicum/ewm/EwmServiceApplication.java
+++ core/ewm-service/src/main/java/ru/practicum/ewm/EwmServiceApplication.java
@@ -3,10 +3,8 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Import;
 import ru.practicum.ewm.client.EventClient;
 import ru.practicum.ewm.client.UserClient;
-import ru.practicum.stats.client.StatsClient;
 
 @EnableFeignClients(
         clients = {
@@ -14,8 +12,12 @@ import ru.practicum.stats.client.StatsClient;
                 EventClient.class
         }
 )
-@Import(StatsClient.class)
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "ru.practicum.ewm",
+                "ru.practicum.stats.client"
+        }
+)
 public class EwmServiceApplication {
 
     public static void main(String[] args) {
