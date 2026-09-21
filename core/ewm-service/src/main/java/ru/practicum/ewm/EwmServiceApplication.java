7ed56f51d6c38fd6e4de6fec14eddc9876ab8ad8
Damir Vafin <damirvafinnailevich@gmail.com>
2026-09-21 15:33:30 +0300

    feat: завершить выделение сервиса комментариев

--- core/ewm-service/src/main/java/ru/practicum/ewm/EwmServiceApplication.java
+++ core/ewm-service/src/main/java/ru/practicum/ewm/EwmServiceApplication.java
@@ -2,22 +2,8 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import ru.practicum.ewm.client.EventClient;
-import ru.practicum.ewm.client.UserClient;
 
-@EnableFeignClients(
-        clients = {
-                UserClient.class,
-                EventClient.class
-        }
-)
-@SpringBootApplication(
-        scanBasePackages = {
-                "ru.practicum.ewm",
-                "ru.practicum.stats.client"
-        }
-)
+@SpringBootApplication
 public class EwmServiceApplication {
 
     public static void main(String[] args) {
